5f4d6256f200c007ec63427a1e9ed2b9b1995cdc
Vincent Liu <vincentmingli@gmail.com>
2023-05-26 17:47:12 -0400

    May 26 2023

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const dataUrl = 'data.json';
+async function Goals() {    
+    const requestUrl = 'https://raw.githubusercontent.com/Nanoscience202/goals/main/data.json';
+    const request = new Request(requestUrl);
+
+    const response = await fetch(request);
+    const data = await response.json();
+}
+
+function displayGoals() {
+    const main = document.querySelector('main');
+    const goalsContainer = document.createElement('div');
+    const itemsContainer = document.createElement('ul');
+
+    itemsContainer.appendChild(document.createElement('li').appendText(data.goals.Webtutorial));
+
+
+
+    main.appendChild(goalsContainer);
+}
