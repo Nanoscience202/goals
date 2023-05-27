a17721486cdbcaef193b6d99ad0d398ba17959a7
Vincent Liu <vincentmingli@gmail.com>
2023-05-26 20:00:05 -0400

    May 26 2023

--- script.js
+++ script.js
@@ -9,11 +9,28 @@ async function Goals() {
 }
 
 function displayGoals(data) {
-    const main = document.querySelector('main');
-    const goalsContainer = document.createElement('div');
-    const itemsContainer = document.createElement('ul');
+    const grid = document.querySelector('#grid-container'); // grid container
+    
+    // name of goal
+    const name = data.webTutorial.name;
+    const nameContainer = document.createElement('div');
+    nameContainer.appendChild(document.createTextNode(name));
+    nameContainer.className = 'name-container'; // add class name to name container: name-container
+    grid.appendChild(nameContainer);
+    
+    // smart property
+    const goalsContainer = document.createElement('div'); // list container
+    const itemsContainer = document.createElement('ul'); // unordered list
+    for (let i = 0; i < data.webTutorial.smart.length; i++) {
+        const item = data.webTutorial.smart[i];
+        const itemElement = document.createElement('li'); // add item to list
+        const itemText = document.createTextNode(item);
+
+        itemElement.appendChild(itemText);
+        itemsContainer.appendChild(itemElement);
+    }
 
     goalsContainer.appendChild(itemsContainer);
-
-    main.appendChild(goalsContainer);
+    goalsContainer.className = 'goals-container'; // add class name to goals container: goals-container
+    grid.appendChild(goalsContainer);
 }
