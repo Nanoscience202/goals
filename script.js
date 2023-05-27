f6d427e06cf24a1c26f511791daea41df374cbdf
Vincent Liu <vincentmingli@gmail.com>
2023-05-27 13:12:17 -0400

    goals

--- script.js
+++ script.js
@@ -13,17 +13,26 @@ function displayGoals(data) {
     
     // name of goal
     const name = data.webTutorial.name;
-    const nameContainer = document.createElement('div');
+    const nameContainer = document.createElement('div'); // name container
     nameContainer.appendChild(document.createTextNode(name));
     nameContainer.className = 'name-container'; // add class name to name container: name-container
     grid.appendChild(nameContainer);
+
+    // description
+    const description = data.webTutorial.description;
+    const descriptionContainer = document.createElement('div'); // description container
+    descriptionContainer.appendChild(document.createTextNode(description));
+    descriptionContainer.className = 'description-container'; // add class name to description container: description-container
+    nameContainer.appendChild(descriptionContainer);
     
     // smart property
     const goalsContainer = document.createElement('div'); // list container
     const itemsContainer = document.createElement('ul'); // unordered list
+    itemsContainer.className = 'list-container'; // add class name to list container: list-container
     for (let i = 0; i < data.webTutorial.smart.length; i++) {
         const item = data.webTutorial.smart[i];
         const itemElement = document.createElement('li'); // add item to list
+        itemElement.className = 'list-item'; // add class name to list item: list-item
         const itemText = document.createTextNode(item);
 
         itemElement.appendChild(itemText);
